c76fd2f72e9452f4509539b6faa0055f10214b86
ui_creeperlv <preknolwedge@gmail.com>
2024-05-19 13:04:19 +0530

    remove `PrismaClient` uness import()

--- src/routes/sites/new-ssl.ts
+++ src/routes/sites/new-ssl.ts
@@ -1,4 +1,4 @@
-import { Application, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { Context } from "elysia";
 import { adminAuth } from "../../utils/auth";
 import { parse } from "../../utils/datarequest";
