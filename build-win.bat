71e4a3b6e1ae7d09e1055b41243ef83b6bf98020
ui_creeperlv <preknolwedge@gmail.com>
2024-05-26 00:51:50 +0530

    fix: attempt 1

---
+++ build-win.bat
@@ -0,0 +1,9 @@
+@setlocal enableextensions
+@cd /d "%~dp0"
+
+set OPENSSL_DIR=C:\Program Files\OpenSSL-Win64
+set PATH=%PATH%;C:\Program Files\OpenSSL-Win64\bin
+@REM set OPENSSL_INCLUDE_DIR=C:\Program Files\OpenSSL-Win64\include
+set OPENSSL_LIB_DIR=C:\Program Files\OpenSSL-Win64\lib\VC\x64\MD
+
+cargo build
