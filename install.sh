e92987beb1b17fe735e2276135093861436f779f
ui_creeperlv <preknolwedge@gmail.com>
2024-05-26 19:26:15 +0530

    Add an install script and add more scripts

---
+++ install.sh
@@ -0,0 +1,53 @@
+# Flix v0.1 Installer
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    curl -fsSL https://bun.sh/install | bash
+
+    sudo ln -s "${HOME}/.bun/bin/bun" /usr/bin
+    sudo ln -s "${HOME}/.bun/bin/bunx" /usr/bin
+
+    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+    nvm install 20
+    nvm use 20
+
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+    export PATH=$PATH:"${HOME}/.cargo/bin";
+
+    mkdir /www
+
+    cd /www
+
+    git clone https://github.com/FluxServer/flix-backend flix
+
+    cd flix
+
+    cargo build --release
+
+    bun install
+
+    bun run ./src/setup.ts
+
+    bunx prisma db push
+    
+    mkdir user_dir
+
+    echo "Installing Nginx"
+
+    ./scripts/install-local-nginx.sh
+
+    sudo ln -s ./src/templates/flix.service /etc/systemd/system
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    curl -fsSL https://bun.sh/install | bash
+
+
+elif [[ "$OSTYPE" == "win32" ]]; then
+    echo "Flix Installation is not supported through bash/sh on Windows Yet"
+else
+    echo "Unknown OS Found";
+fi
--- src/setup.ts
+++ src/setup.ts
@@ -1,5 +1,23 @@
-import { prisma } from "./db"
-import { mkdirSync } from "node:fs";
+import { join } from "path";
 
-await prisma.$connect();
-console.log("🗄️ Database was connected!")
+let dbPath = join("/www" , "flix" , "user_dir" , "database.db")
+let envPath = join("/www" , "flix" , ".env");
+let flix_rs_runtime = join("/www" , "flix" , "target" , "release" , "flix")
+
+let email = prompt("E-Mail Address? ");
+let defaultPort = prompt("Flix Listening Port? (Default: 3000) : " , "3000")
+
+let envRAW = `EMAIL=${email}
+# Runtimes
+FLIXRS_RUNTIME=${flix_rs_runtime}
+UNZIP_RUNTIME=/usr/bin/unzip
+# Database
+DATABASE_URL="file:${dbPath}"
+# Site Ports
+PORT=${defaultPort}
+# Mysql Informations
+HOST=127.0.0.1
+USERNAME=
+PASSWORD=`;
+
+await Bun.write(envPath, envRAW);
