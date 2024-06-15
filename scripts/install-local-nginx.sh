7db2f8eeb969cb4ab9b683a1bf1b090f14dfc66c
indprtech <indprtech@gmail.com>
2024-06-16 00:33:21 +0530

    fix: nginx issues and use launchpad instead of nginx repo to install nginx-full

--- scripts/install-local-nginx.sh
+++ scripts/install-local-nginx.sh
@@ -2,11 +2,7 @@ mkdir /www
 sudo ln -s "${PWD}" /www/
 
 sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring -y
-curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
-    | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
-echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
-http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
-    | sudo tee /etc/apt/sources.list.d/nginx.list
+
 sudo apt update
 sudo apt install nginx-full -y
 source /etc/os-release
